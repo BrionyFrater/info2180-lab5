5195e3c281906892d31b892bb550d24cd674065f
Briony Frater <briefrater@gmail.com>
2022-11-08 00:25:15 -0500

    Implemented search funtionality in world.php and world.js

--- world.js
+++ world.js
@@ -1,3 +1,26 @@
 window.onload = function(){
    
+    let lookUp = document.getElementById("lookup");
+    let resultDiv = document.getElementById("result");
+    
+
+    lookUp.addEventListener("click", () => {
+        let country = document.getElementById("country").value;
+
+        fetch("world.php?country=" + String(country))
+            .then((response)=>{
+                if(response.ok){
+                    return response.text();
+                }else{
+                    return Promise.reject("An error has occured");
+                }
+            })
+            .then((data)=>{
+                resultDiv.innerHTML = data;
+            })
+            .catch((error)=>{
+                console.log("Sorry an error has occured: " + error);
+            })
+
+    });
 };
--- world.php
+++ world.php
@@ -6,14 +6,15 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
+
 $country = $_GET['country'];
-echo $country;
 
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
